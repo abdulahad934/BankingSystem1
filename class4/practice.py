25dcbc7a266825276b6daaa4863eacea88482148
abdulahad934 <adbulahad934@gmail.com>
2025-07-29 02:24:20 +0600

    update

---
+++ class4/practice.py
@@ -0,0 +1,15 @@
+list_item = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+list_item.reverse()
+print(list_item)
+
+list_rev = list(reversed(list_item))
+print(list_rev)
+
+    
+
+# list_len = len(list_item)
+# list_max = max(list_item)
+# print(list_max)
+
+# print(list_len)

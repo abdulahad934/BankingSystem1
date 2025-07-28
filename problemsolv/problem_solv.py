a6444f4abb09d60e3ac658b7b53ba205d49a7d28
abdulahad934 <adbulahad934@gmail.com>
2025-07-29 01:23:25 +0600

    Update

--- problemsolv/problem_solv.py
+++ problemsolv/problem_solv.py
@@ -19,8 +19,28 @@ print(list_fruits2)
 
 list_num = [1, 2, 3, 4, 5]
 
+list_num.append([20, 30, 40, 50, 60])
+list_num.insert(2, 200)
+print(list_num)
+
 new_item = [6, 7, 8, 9]
 
 for item in new_item:
-    list_num += [item]
+    list_num +=  [item]
     print(list_num)
+    
+    
+
+
+
+list_collection = ["shirt", "pant"]
+
+new_collection = ["T-shirt", "Trawzer"]
+
+all_products = list_collection + new_collection
+print(all_products)
+
+
+all_products = [produc for produc in list_collection] + [product for product in new_collection]
+
+print(all_products)

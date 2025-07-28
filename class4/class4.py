4de324cf86f38f0bc8b3f7d6d6ee85af9778fd68
abdulahad934 <adbulahad934@gmail.com>
2025-07-29 00:18:22 +0600

    Second Update

--- class4/class4.py
+++ class4/class4.py
@@ -2,6 +2,12 @@ list_num = [1, 2, 3, 4, 5 ]
 
 list_num = list_num + [6, 7, 8, 9, 10]
 
+list_num.append([40, 50, 70, 80, 100])
+list_num.extend([200, 300])
+
+list_num += [90, 91, 92, 93, 94, 95]
+print(list_num)
+
 print(list_num)
 
 new_num = [11, 12, 13, 14, 15]
